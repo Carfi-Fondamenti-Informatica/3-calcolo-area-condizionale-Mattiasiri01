0dbc00ece4fdda65116e4abec30e1e112249dce3
Mattiasiri01 <100609727+Mattiasiri01@users.noreply.github.com>
2022-03-29 14:23:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
 
+
 int main() {
-   return 0;
+    float a,b;
+    cin>> a >>b;
+    int opzione;
+    cin>> opzione;
+    float Atriangolo=0, Aquadrato=0, Arettangolo=0;
+            switch( opzione ){
+        case 0:
+            Atriangolo= ((a*b)/2);
+            cout<< Atriangolo <<endl;
+                    break;
+                case 1:
+                    Aquadrato= (a*a);
+                    cout<< Aquadrato <<endl;
+                    break;
+                case 2:
+                    Arettangolo= (a*b);
+                    cout<< Arettangolo<<endl;
+                default:
+                    cout<< "opzione non valida"<<endl;
+            }
+    return 0;
 }
+
